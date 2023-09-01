f7b5f58b5741a2e12e6828162562cb1bca3047f3
Docvelkv <docvel@rambler.ru>
2023-09-02 00:37:55 +0300

    Задача 1 - решена

---
+++ src/main/java/Task1.java
@@ -0,0 +1,23 @@
+// Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float),
+// и возвращает введенное значение.
+// Ввод текста вместо числа не должно приводить к падению приложения,
+// вместо этого, необходимо повторно запросить у пользователя ввод данных.
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        boolean flag = true;
+        while (flag){
+            try {
+                System.out.print("Введите дробное число: ");
+                float num = Float.parseFloat(scan.next());
+                flag = false;
+                System.out.println(num);
+            } catch (NullPointerException | NumberFormatException ex) {
+                System.out.print("Неправильный ввод. ");
+            }
+        }
+    }
+}
